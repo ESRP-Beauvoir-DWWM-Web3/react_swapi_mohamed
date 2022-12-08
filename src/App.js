5fd78030fa874e60de4cbcd8efcdff9ab9dbe370
Mohmel <melmed413@gmail.com>
2022-12-08 10:21:08 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import People from './components/People';
+
+
+function App() {
+  return (
+    <People />
+  );
+}
+
+export default App;
---
+++ src/components/People.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react'; 
+import axios from "axios";
+
+
+
+export default function People(){
+
+    return <h1>Hello Word</h1>
+}
